151b57a0917d34c9fac7e8c5fe5e997d73a27634
Sourabh Jain <42956699+jainsourab99@users.noreply.github.com>
2024-10-20 17:27:17 +0530

    List View

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,60 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: const HonePage(),
+    );
+  }
+}
+
+class HonePage extends StatelessWidget {
+  const HonePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List View"),
+      ),
+      body: ListView.separated(
+          separatorBuilder: (context, seprator) {
+            return Container(
+              height: 20,
+              width: double.infinity,
+              color: Colors.grey,
+            );
+          },
+          itemCount: 200,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              title: Text("UserName"),
+              subtitle: Text("Dome description"),
+              trailing: Icon(Icons.delete),
+            );
+          }
+          // children: [
+          //   ListTile(
+          //     leading: Container(
+          //       width: 50,
+          //       height: 50,
+          //       decoration: BoxDecoration(
+          //         color: Colors.grey,
+          //         shape: BoxShape.circle,
+          //       ),
+          //     ),
+          //     title: Text("UserName"),
+          //     subtitle: Text("Dome description"),
+          //     trailing: Icon(Icons.delete),
+          //   ),
+          // ],
+          ),
     );
   }
 }
